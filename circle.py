67a798a61b3203733ed852baeea1e071de4d1c6a
Nuha Mohammed <nuha14m@gmail.com>
2019-06-01 14:56:55 -0400

    Update circle.py

--- circle.py
+++ circle.py
@@ -26,22 +26,6 @@ def binarycon(n, lfinal):
     n1=n1[2:]
     if lfinal> len(n1): n1 = '0'*int(lfinal-len(n1))+n1
     return n1
-
-def train(n, num_input):
-    n = binarycon(n, int(math.pow(2,num_input)))
-    b=0
-    w=(0,)*num_input
-    train_set= truth_table(num_input, n)
-    x_1=tuple()
-    for i in range(100):
-        for key in train_set.keys():
-            x,y= key, train_set[key]
-            y_1 = step(dot(w,x)+b)
-            err = int(y)-y_1
-            x_1 = (int(i) * err for i in x)
-            w=  tuple(map(sum,zip(w,x_1)))
-            b+=err
-    return w,b
    
 def circle_perceptron(x,y):
     A= perceptron(sigmoid, (0,1), 1, (x,y))
@@ -56,15 +40,19 @@ def circleeqn(x,y):
  
 if __name__ == "__main__":
     threshold=0.3399
-    
+    correct=0
     for x in np.arange(-20, 20):
         for y in np.arange(-20, 20):
             P= circle_perceptron(x*0.1,y*0.1)
             val=circleeqn(x*0.1,y*0.1)
-            if P<=threshold and val>1: plt.plot([x*0.1], [y*0.1],'bo', markersize=2)
-            elif P<=threshold and val<=1: plt.plot([x*0.1], [y*0.1],'yo', markersize=2)
+            if P<=threshold and val>1: 
+                plt.plot([x*0.1], [y*0.1],'bo', markersize=2)
+                correct+=1
+            elif P<=threshold and val<=1: 
+                plt.plot([x*0.1], [y*0.1],'yo', markersize=2)
+                correct+=1
             elif P>threshold and val<=1: plt.plot([x*0.1], [y*0.1],'go', markersize=2)
             elif P>threshold and val>1: plt.plot([x*0.1], [y*0.1], 'co', markersize=2)
-
+    print(correct)
     plt.show()
        
